4d46bc5d6fb70bc6ed4771d2ac8788e490d91bb0
yeyiqiushui <119306142+yeyiqiushui@users.noreply.github.com>
2023-03-20 17:41:36 +0800

    Add files via upload

---
+++ 作业一/作业一/作业一.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+class Complex
+{
+public:
+    Complex()
+    {
+        real1 = imaginary2 = 0;
+        real2 = imaginary1 = 0;
+    }
+    void addComplex(double a,double b,double c,double d)
+    {
+        real1 = a + c;
+        imaginary1 = b + d;
+        cout << "<" << real1 <<","<< imaginary1 << ">" << endl;
+    }
+    void subComplex(double a, double b, double c, double d)
+    {
+        real2 = a - c;
+        imaginary2 = b - d;
+        cout << "<" << real2 << "," << imaginary2 << ">" << endl;
+    }
+private:
+    double real1;
+    double imaginary1;
+    double real2;
+    double imaginary2;
+};
+int main()
+{
+    double a=0, b=0,c=0,d=0;
+    class Complex add;
+    cin >> a >> b >> c >> d;
+    cout << "<" << a << "," << b << ">+<" << c << "," << d << ">=";
+    add.addComplex(a, b, c, d);
+    class Complex sub;
+    cin >> a >> b >> c >> d;
+    cout << "<" << a << "," << b << ">-<" << c << "," << d << ">=";
+    sub.subComplex(a, b, c, d);
+}
---
+++ 作业二/作业二/作业二.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+using namespace std;
+class Time
+{
+public:
+    void setnoteTime(int br)
+    {
+        note = br;
+    }
+    void tickTime(int hour, int min, int second)
+    {
+        for (int i = 0;i < note; i++)
+        {
+            cout << hour << ":" << min << ":" << second;
+                second++;
+                if (second > 59)
+                {
+                    min++;
+                    second = 0;
+                }
+                if (min > 59)
+                {
+                    hour++;
+                    min = 0;
+                }
+                if (hour > 23)
+                {
+                    hour = 0;
+                }
+                if (hour < 10)
+                {
+                    cout << "0" << hour << ":";
+                }
+                else
+                {
+                    cout << hour << ":";
+                }
+                if (min < 10)
+                {
+                    cout << "0" << min << ":";
+                }
+                else
+                {
+                    cout << min << ":";
+                }
+                if (second < 10)
+                {
+                    cout << "0" << second ;
+                }
+                else
+                {
+                    cout << second ;
+                }
+            if (hour < 13)
+                cout << "am" << endl;
+            else
+                cout << "pm" << endl;
+        }
+    }
+private:
+    int note;
+};
+int main()
+{
+    int hour = 0, min = 0, second = 0;
+    cin >> hour >> min >> second;
+    int br;cin >> br;
+    class Time tick;
+    tick.setnoteTime(br);
+    tick.tickTime(hour, min, second);
+}
